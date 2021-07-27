fb4eaadbbce3b4b03f05554ed7bf5fc40a3aab5d
Daun jo <jodawooooon@naver.com>
2021-07-28 00:49:36 +0900

    [#11] feat: :sparkles: repo 수정

--- backend/src/main/java/com/ssafy/db/repository/chat/ConferenceHistoryRepository.java
+++ backend/src/main/java/com/ssafy/db/repository/chat/ConferenceHistoryRepository.java
@@ -1,10 +1,11 @@
 package com.ssafy.db.repository.chat;
 
+import com.ssafy.db.entity.chat.ConferenceHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ConferenceHistoryRepository extends JpaRepository<ConferenceHistoryRepository, Long> {
+public interface ConferenceHistoryRepository extends JpaRepository<ConferenceHistory, Long> {
 
 
 }
