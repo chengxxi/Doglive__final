1e459a0bace7cb9d8bf7cfd00e8d356dfe64719f
Daun jo <jodawooooon@naver.com>
2021-08-02 15:36:47 +0900

    [#1] feat: :sparkles: 입양임보 게시판 컨트롤러 생성

--- backend/src/main/java/com/ssafy/api/controller/BoardController.java
+++ backend/src/main/java/com/ssafy/api/controller/BoardController.java
@@ -1,15 +1,14 @@
 package com.ssafy.api.controller;
 
 
+import com.ssafy.api.request.BoardRegisterPostReq;
+import com.ssafy.api.request.UserRegisterPostReq;
 import com.ssafy.api.response.BoardListGetRes;
 import com.ssafy.api.response.UserLoginPostRes;
 import com.ssafy.api.service.AdoptService;
 import com.ssafy.common.model.response.BaseResponseBody;
 import com.ssafy.db.entity.board.Board;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -43,7 +42,23 @@ public class BoardController {
         return ResponseEntity.ok(BoardListGetRes.of(200, "Success", boardList, boardList.size()));
     }
 
-    @DeleteMapping("")
+    @PostMapping()
+    @ApiOperation(value = "입양/임보 공고 등록", notes = "입양/입양 공고를 삭제한다")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "성공"),
+            @ApiResponse(code = 401, message = "인증 실패"),
+            @ApiResponse(code = 404, message = "사용자 없음"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
+    public ResponseEntity<? extends BaseResponseBody> registerAdoptBoard(@RequestBody @ApiParam(value="공고 등록 정보", required = true)BoardRegisterPostReq boardRegisterPostReq){
+        Board board = adoptService.registerAdoptBoard(boardRegisterPostReq);
+        System.out.println(board);
+        return ResponseEntity.status(200).body(BaseResponseBody.of(200, "공고가 정상적으로 등록되었습니다"));
+    }
+
+
+
+    @DeleteMapping("/{boardId}")
     @ApiOperation(value = "입양/임보 공고 삭제", notes = "입양/입양 공고를 삭제한다")
     @ApiResponses({
             @ApiResponse(code = 204, message = "성공"),
@@ -51,11 +66,9 @@ public class BoardController {
             @ApiResponse(code = 404, message = "사용자 없음"),
             @ApiResponse(code = 500, message = "서버 오류")
     })
-    public ResponseEntity<BoardListGetRes> deleteAdoptBoard(){
-        List<Board> boardList = adoptService.getAdoptBoardList();
-
-
-        return ResponseEntity.ok(BoardListGetRes.of(200, "Success", boardList, boardList.size()));
+    public ResponseEntity<? extends BaseResponseBody> deleteAdoptBoard(@PathVariable("boardId") String id){
+        adoptService.deleteAdoptBoard(Long.parseLong(id));
+        return ResponseEntity.status(200).body(BaseResponseBody.of(200, "공고가 정상적으로 삭제되었습니다"));
     }
 
 }
