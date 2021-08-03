8c42b83fc6331facad4af8e57c021ba3157aa3d9
Daun jo <jodawooooon@naver.com>
2021-08-04 00:19:57 +0900

    [#9] feat: :sparkles: 기존 adopt 푸시

---
+++ frontend/src/views/adopt/components/adopt-list.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+  <!-- <el-row> -->
+    <h1>adopt cards로 구성된 전체 list</h1>
+
+    <AdoptCard />
+      <!-- v-for="(card, idx) in cards"
+      :key="idx"
+      :card="card"
+    /> -->
+
+  <!-- </el-row> -->
+  </div>
+</template>
+
+
+
+<style scoped>
+
+
+</style>
+
+
+
+<script>
+import AdoptCard from "@/views/adopt/components/adopt-card";
+
+export default {
+  name: 'adopt-list',
+  components: { AdoptCard },
+  setup () {
+
+  }
+}
+</script>
