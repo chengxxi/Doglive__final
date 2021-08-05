28b81221eedf128f31a9a47d51bcef654cb9abf3
hyewon <hiii456@daum.net>
2021-08-05 11:41:34 +0900

    [#12] wip: :gear: actions.js에 작성글 불러오기 메서드 추가

--- frontend/src/views/main/store/actions.js
+++ frontend/src/views/main/store/actions.js
@@ -22,9 +22,17 @@ export function requestKakaoLogout({ state }){
   return $axios.get(url)
 }
 
+// 사용자 북마크 리스트를 불러오기
 export function requestBookmarkList({ state },payload){
   const url = '/api/v1/users/bookmark/' + payload;
   let body = payload;
-  console.log(body);
   return $axios.get(url)
 }
+
+// 사용자가 작성한 글 리스트 불러오기
+export function requestUserPostList({ state },payload){
+  const url = '/api/v1/board/myboard/' + payload;
+  let body = payload;
+  return $axios.get(url)
+}
+
