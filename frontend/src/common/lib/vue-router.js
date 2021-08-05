1392c139f0fae4b33e4ae7b10c70a57494261f8e
Daun jo <jodawooooon@naver.com>
2021-08-05 14:14:31 +0900

    [#13] feat: :sparkles: 재병합

--- frontend/src/common/lib/vue-router.js
+++ frontend/src/common/lib/vue-router.js
@@ -77,6 +77,7 @@ const routes = [
       },
     ]
   },
+  {
     path: '/adopt',
     name: 'Adopt',
     component: Adopt
@@ -96,6 +97,7 @@ const routes = [
     name: 'AdoptRegister',
     component : AdoptRegister
   }
+
 ]
 
 const router = createRouter({
