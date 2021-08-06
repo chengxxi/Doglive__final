400cb59f12eaa3d91e84ad5b7833919af660b38e
Daun jo <jodawooooon@naver.com>
2021-08-06 12:56:16 +0900

    feat: :sparkles: 1차 배포 테스트 프론트 재수정

--- frontend/src/views/adopt/adopt-register.vue
+++ frontend/src/views/adopt/adopt-register.vue
@@ -383,7 +383,7 @@ export default {
       const data = {
         thumbnailUrl: "",
         filePath: [],
-        dogName: this.state.name,
+        dogName: this.ruleForm.name,
         boardType: Number(this.ruleForm.type),
         userId: this.state.userId.userId,
         mbti: mbti,
