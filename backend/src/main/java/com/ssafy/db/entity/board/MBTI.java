f33e5fc5cb1b479985a677fea105a7c4f4dc7ed1
Daun jo <jodawooooon@naver.com>
2021-08-08 00:25:21 +0900

    [#21] wip: :gear: mbti entity, repository 구현

---
+++ backend/src/main/java/com/ssafy/db/entity/board/MBTI.java
@@ -0,0 +1,28 @@
+package com.ssafy.db.entity.board;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="mbti", schema = "board")
+@Getter
+@Setter
+public class MBTI extends BaseEntity{
+
+    @Column(length = 20)
+    String name;
+
+
+    String title;
+
+    String desc;
+
+    @Column(name="image_url")
+    String imageUrl;
+}
