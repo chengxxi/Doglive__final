6ae632a1ba8220f98fbd2714d6c855c6bce09e06
DESKTOP-KVCQHCD\multicampus <heeya121hs@gmail.com>
2021-08-10 15:03:11 +0900

    [#25] style: :art: MBTI UI design

--- frontend/src/views/mbti/components/mbti-description.vue
+++ frontend/src/views/mbti/components/mbti-description.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="box" style="margin-left:30px; margin:right:30px; display: flex; justify-content: center;">
-    <h3 class="mt-1 mb-3" style="font-weight:600; background-color:#fbe997">
+    <h3  style="font-weight:005; background-color:#fbe997">
+      <!-- class="mt-1 mb-3" -->
       나와 맞는 성향의 강아지 찾기 테스트
     </h3>
   </div>
@@ -8,6 +9,21 @@
 </template>
 
 <style scoped>
+/* 페이지 만들 때, 이 구조가 기준이 됩니다! (양옆 여백 10%, 위 여백 50px) */
+.main-body {
+  width: 100%;
+  margin-left: 10%; /* 페이지 양옆 200px여백 -> 10% */
+  margin-right: 10%;
+}
+.main-padding {
+  padding-top: 50px;
+}
+
+.title {
+  font-size: 2.5rem;
+  font-weight: normal;
+}
+
 .box {
   margin-top: 30px;
   margin-bottom: 40px;
--- frontend/src/views/mbti/components/mbti-question.vue
+++ frontend/src/views/mbti/components/mbti-question.vue
@@ -1,20 +1,24 @@
 <template>
-  <div class="box" style="margin-left:30px; margin:right:30px; display: flex; justify-content: center;">
-    <h3 class="mt-1 mb-3" style="font-weight:600; background-color:#fbe997">
-      나와 맞는 성향의 강아지 찾기 테스트
-    </h3>
+  <div>
+    questions
   </div>
 
 </template>
 
 <style scoped>
-.box {
-  margin-top: 30px;
-  margin-bottom: 40px;
-  margin-left: 50px;
-  margin-right: 50px;
-  padding: 40px;
-  background-color: #f9f0e7;
+/* 페이지 만들 때, 이 구조가 기준이 됩니다! (양옆 여백 10%, 위 여백 50px) */
+.main-body {
+  width: 100%;
+  margin-left: 10%; /* 페이지 양옆 200px여백 -> 10% */
+  margin-right: 10%;
+}
+.main-padding {
+  padding-top: 50px;
+}
+
+.title {
+  font-size: 2.5rem;
+  font-weight: normal;
 }
 </style>
 
