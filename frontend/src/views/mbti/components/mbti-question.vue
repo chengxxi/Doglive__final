9096d90984f8b7b4945eadb3f059c468c5c173aa
DESKTOP-KVCQHCD\multicampus <heeya121hs@gmail.com>
2021-08-11 14:56:03 +0900

    [#25] style: :art: MBTI question designing

--- frontend/src/views/mbti/components/mbti-question.vue
+++ frontend/src/views/mbti/components/mbti-question.vue
@@ -19,7 +19,7 @@
     </p>
 
     <el-radio-group v-model="radio" class="rating">
-      <el-radio-button :label="1" size="medium">매우 아니다</el-radio-button> <!-- 매우 아니다 -->
+      <el-radio-button :label="1" class="radioBtn" size="medium">매우 아니다</el-radio-button> <!-- 매우 아니다 -->
       <el-radio-button :label="2" size="small">조금 아니다</el-radio-button> <!-- 조금 아니다 -->
       <el-radio-button :label="3" size="mini">보통이다</el-radio-button> <!-- 보통이다 -->
       <el-radio-button :label="4" size="small">조금 그렇다</el-radio-button> <!-- 조금 그렇다 -->
@@ -32,13 +32,18 @@
       혼날 때, 한껏 주눅들어 있어요.
     </p>
 
-    <el-radio-group v-model="radio3" clearable class="rating">
-      <el-radio-button :label="1" class="radioBtn"></el-radio-button> <!-- 매우 아니다 -->
-      <el-radio-button :label="2" class="radioBtn"></el-radio-button> <!-- 조금 아니다 -->
-      <el-radio-button :label="3" class="radioBtn"></el-radio-button> <!-- 보통이다 -->
-      <el-radio-button :label="4" class="radioBtn"></el-radio-button> <!-- 조금 그렇다 -->
-      <el-radio-button :label="5" class="radioBtn"></el-radio-button> <!-- 매우 그렇다 -->
-    </el-radio-group>
+    <el-radio-item class="align-items-center">
+      <el-radio-group v-model="radio3" class="rating">
+        <span>비동의</span>
+        <el-radio-button :label="1" class="radioBtn-lg-lf"></el-radio-button> <!-- 매우 아니다 -->
+        <el-radio-button :label="2" class="radioBtn-md-lf"></el-radio-button> <!-- 조금 아니다 -->
+        <el-radio-button :label="3" class="radioBtn-sm"></el-radio-button> <!-- 보통이다 -->
+        <el-radio-button :label="4" class="radioBtn-md-rt"></el-radio-button> <!-- 조금 그렇다 -->
+        <el-radio-button :label="5" class="radioBtn-lg-rt"></el-radio-button> <!-- 매우 그렇다 -->
+        <span>동의</span>
+      </el-radio-group>
+    </el-radio-item>
+
   </div>
 
 
@@ -74,26 +79,63 @@
 .question {
   font-weight: 500;
   margin-bottom: 40px;
+  /* align-items: center; */
 }
 
 .demonstration {
-  font-size: 1.5rem;
+  font-size: 1.25rem;
 }
 
 .rating {
   font-size: 2rem;
+  align-items: center;
 }
 
-.radioBtn {
-  content: '';
-  width: 1.25em;
-  height: 1.25em;
-  appearance: none;
-  /* font-size: 3rem; */
-  border: 2px solid salmon;
-  border-radius: 50%;
-  margin: 0 10px;
+.rating > span {
+  font-size: 1rem;
 }
+
+.radioBtn-lg-lf {
+  width: 3rem;
+  height: 3rem;
+  border: 2px solid #755744;
+  border-radius: 50%;
+  margin: 0 25px;
+}
+
+.radioBtn-md-lf {
+  width: 2rem;
+  height: 2rem;
+  border: 2px solid #8a6f5e;
+  border-radius: 50%;
+  margin: 0 25px;
+}
+
+.radioBtn-sm {
+  width: 1rem;
+  height: 1rem;
+  border: 2px solid #E9CDA4;
+  border-radius: 50%;
+  margin: 0 25px;
+}
+
+.radioBtn-md-rt {
+  width: 2rem;
+  height: 2rem;
+  border: 2px solid #D7AEA4;
+  border-radius: 50%;
+  margin: 0 25px;
+}
+
+.radioBtn-lg-rt {
+  width: 3rem;
+  height: 3rem;
+  border: 2px solid #c58676;
+  border-radius: 50%;
+  margin: 0 25px;
+}
+
+
 
 
 /* radio 안에 체크하는 원 부분 삭제 */
@@ -125,7 +167,7 @@ export default {
       // same as { 2: '#99A9BF', 4: { value: '#F7BA2A', excluded: true }, 5: '#FF9900' }
       // iconClasses: ref(['icon-rate-face-1', 'icon-rate-face-2', 'icon-rate-face-3'])
       // same as { 2: 'icon-rate-face-1', 4: { value: 'icon-rate-face-2', excluded: true }, 5: 'icon-rate-face-3' }
-      radio: ref(3),
+      radio: ref(null),
       radio2: ref(null),
       radio3: ref(null),
     };
