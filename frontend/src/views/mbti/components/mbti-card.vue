e17fc0f6b8f6af52d771778882bff6851f11c186
DESKTOP-KVCQHCD\multicampus <heeya121hs@gmail.com>
2021-08-12 16:39:05 +0900

    [#25] wip: :gear: MBTI card 디자인 중

--- frontend/src/views/mbti/components/mbti-card.vue
+++ frontend/src/views/mbti/components/mbti-card.vue
@@ -1,6 +1,51 @@
 <template>
-  <el-card>
-    esfefse
+  <el-card shadow="hover" style="background-color:#f9f4f0;">
+    <el-row style="margin-top:15px; margin-right:15px; margin-left:15px;">
+      <el-tag
+        class="mb-2"
+        effect="dark"
+        size="small"
+        style="height:20px; background:linear-gradient( to right, #D7AFA4, #E9CDA4, #B4D9A7, #87CEDC ); border:none; font-weight:600;  color:#FFFFFF; float:left;"
+        > MBTI
+        <!-- {{ card.mbti }} -->
+        </el-tag>
+      <el-tag
+        class="mb-2"
+        color="#D7AFA4"
+        effect="dark"
+        size="small"
+        style="border:none; font-size:14px; height:20px; font-weight:700; border-radius: 30px; color:#FFFFFF; float:right; "
+        > Name
+        <!-- {{ card.boardId.type.name }} -->
+      </el-tag>
+    </el-row>
+
+    <el-row
+      style="margin-top:5px; display:flex; display: flex; justify-content: center;"
+    >
+      <img src="https://placedog.net/500/500?random" class="image" />
+    </el-row>
+
+    <el-row
+      style="margin-top:10px; margin-right:15px; margin-left:15px; vertical-align: middle; "
+    >
+      <h3 style="font-weight:800; float:right;" class="mb-0">
+        dogName <!-- {{ card.dogName }} -->
+      </h3>
+      <p style="font-size:13px; font-weight:600; margin-bottom:0px;">
+        Title
+        <!-- {{
+          card.boardId.title.length < 12
+            ? card.boardId.title
+            : card.boardId.title.substr(0, 10) + "..."
+        }} -->
+      </p>
+
+      <!-- <p style="font-size:10px; margin-top:0px; color:#727272;">
+        {{ card.gender.name }} / {{ card.age.name.split("(")[0] }} /
+        {{ card.weight.name.split("(")[0] }}
+      </p> -->
+    </el-row>
   </el-card>
 </template>
 
--- frontend/src/views/mbti/components/mbti-result.vue
+++ frontend/src/views/mbti/components/mbti-result.vue
@@ -1,12 +1,40 @@
 <template>
-  <span v-for='(card, idx) in boardList' :key='idx'>
-    <el-col :span='6'>
-      <MbtiCard/>
-    </el-col>
-  </span>
+  <div>
+    <div class="box">
+      <h3 class="description">
+        🐕 강아지 MBTI란? 🐾
+      </h3>
+      <br>
+      <p>MBTI 개별 유형 설명 페이지</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium at illo molestias possimus nihil minus? Magnam dolor, voluptas minus vitae ab odio excepturi velit provident unde saepe itaque reiciendis nesciunt!</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem dolor laboriosam, neque veritatis maiores culpa quis, molestiae quisquam eius necessitatibus dolorum. Itaque esse laboriosam, similique atque tempora corrupti architecto asperiores.</p>
+      <!-- 상세 소개 내용 수정 필요 -->
+    </div>
+
+
+
+    <MbtiCard/>
+
+
+  </div>
+
 </template>
 
 <style scoped>
+.box {
+  margin-top: 30px;
+  margin-bottom: 30px;
+  margin-left: 50px;
+  margin-right: 50px;
+  padding: 40px;
+  background-color: #f9f0e7;
+}
+
+.description {
+  font-size: 1.5rem;
+  font-weight: 600;
+}
+
 </style>
 
 
