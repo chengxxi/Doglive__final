0bd926e076b9c71c9df9f2dd21c29cbe0671959d
이영주 <clleo97@naver.com>
2021-08-13 00:43:41 +0900

    [#16] weap: :gear: openvidu 서버 url 변경

--- frontend/src/views/main/components/conference.vue
+++ frontend/src/views/main/components/conference.vue
@@ -124,7 +124,7 @@ import UserVideo from "../../conferences/UserVideo.vue";
 import ConferenceChat from "../../conferences/conferenceChat.vue";
 
 axios.defaults.headers.post["Content-Type"] = "application/json";
-const OPENVIDU_SERVER_URL = "https://i5a501.p.ssafy.io:4443";
+const OPENVIDU_SERVER_URL = "https://i5a501.p.ssafy.io:8443";
 const OPENVIDU_SERVER_SECRET = "doglivedoggi";
 export default {
   name: "Conference",
