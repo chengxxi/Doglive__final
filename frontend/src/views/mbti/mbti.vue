309ec91a54338e452d3d1e12e754554a2543d8a3
DESKTOP-KVCQHCD\multicampus <heeya121hs@gmail.com>
2021-08-12 11:04:52 +0900

    [#25] feat: :sparkles: MBTI Intro 페이지

--- frontend/src/views/mbti/mbti.vue
+++ frontend/src/views/mbti/mbti.vue
@@ -8,12 +8,15 @@
 
       </div>
 
-      <MbtiDescription />
+      <MbtiIntro />
 
-      <!-- if 사용자가 아직 테스트 전이면 -->
-      <MbtiQuestion />
-      <!-- if 사용자의 테스트 결과가 있으면 -->
-      <!-- <MbtiResult /> -->
+
+
+      <!-- 테스트 하기 버튼 -->
+        <!-- <MbtiDescription /> -->
+        <!-- <MbtiQuestion /> -->
+      <!-- 유형 알아보기 버튼 -->
+        <!-- <MbtiResult /> -->
 
     </div>
   </div>
@@ -40,16 +43,12 @@
 
 
 <script>
-import MbtiDescription from './components/mbti-description.vue'
-import MbtiQuestion from './components/mbti-question.vue'
-// import MbtiResult from './components/mbti-result.vue'
+import MbtiIntro from './components/mbti-intro.vue'
 
 export default {
   name: 'mbti',
   components: {
-    MbtiDescription,
-    MbtiQuestion,
-    // MbtiResult
+    MbtiIntro
   },
 }
 </script>
