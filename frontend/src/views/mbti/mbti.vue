5db72efc40ffe733587b5f902f464023a15988e4
DESKTOP-KVCQHCD\multicampus <heeya121hs@gmail.com>
2021-08-12 09:11:42 +0900

    [#25] style: :art: remove box shadow effect

--- frontend/src/views/mbti/mbti.vue
+++ frontend/src/views/mbti/mbti.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main-body main-padding">
-    <el-card class="box-card " style="width:100%; border:none;">
+    <div class="box-card " style="width:100%; border:none;">
       <div style="margin-top:50px">
         <h1 class="title">나와 맞는 강아지 찾기</h1>
         <p>강아지 성향 기반 MBTI를 통한 나와 맞는 성향의 강아지 찾기</p>
@@ -15,7 +15,7 @@
       <!-- if 사용자의 테스트 결과가 있으면 -->
       <!-- <MbtiResult /> -->
 
-    </el-card>
+    </div>
   </div>
 </template>
 
