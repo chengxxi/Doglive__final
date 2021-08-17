8322435f76fecf6c05f86c6ae4eb693ecb5ee3c8
DESKTOP-KVCQHCD\multicampus <heeya121hs@gmail.com>
2021-08-17 13:50:23 +0900

    [#25] feat: :sparkles: MBTI Card 수정

--- frontend/src/views/mbti/components/mbti-card.vue
+++ frontend/src/views/mbti/components/mbti-card.vue
@@ -1,82 +1,35 @@
 <template>
-  <div class="row h-100 justify-content-center pt-3">
-            <div
-              class="col-12 col-sm-9 col-md-4 mt-4"
-              v-for="(card, idx) in state.communityList"
-              :key="idx"
-              :card="card"
-              @click="goCommunity"
-              style="cursor:pointer;"
-            >
-              <div class="card h-100 hover-top rounded-3 shadow">
-                <div class="card-body">
-                  <el-tag
-                    v-if="card.category == '입양일기'"
-                    color="#D7AFA4"
-                    effect="dark"
-                    style="border:none; border-radius: 30px; font-size:14px;"
-                    >{{ card.category }}</el-tag
-                  >
-                  <hr class="text-100" />
+  <div class="row h-100 justify-content-center pt-3 mbtiCard">
 
-                  <div class="d-flex align-items-center">
-                    <img
-                      style="width:100%; height:300px;
-                    margin-left:5px margin-right:5px"
-                      :src="card.fileList[0]"
-                      alt="blog"
-                    />
-                  </div>
-                  <div class="my-3 fs--1">
-                    <div class="user">
-                      <img class="user-profile" :src="card.profileImageUrl" />
-                      <span class="mb-0 fw-bold">{{ card.name }}</span>
-                    </div>
-                    <div
-                      style="padding-top:10px; margin-left:10px; margin-right:10px;"
-                    >
-                      <p class="mb-0 fw-bold" style="font-size:12pt;">
-                        {{ card.title }}
-                      </p>
-                      <p class=" mb-0" style="font-size:11pt;">
-                        <span>{{
-                          card.description.length < 28
-                            ? card.description
-                            : card.description.substr(0, 27) + ".."
-                        }}</span>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+    <div class="card h-100 hover-top rounded-3 shadow">
+      <div class="card-body">
+        <hr class="text-100" />
+
+        <div class="d-flex align-items-center">
+          <img
+            style="width:100%; height:300px; margin-left:5px margin-right:5px"
+            :src="card.imageUrl"
+            alt="blog"
+          />
+        </div>
+
+        <div class="my-3 fs--1">
+          <div style="padding-top:10px; margin-left:10px; margin-right:10px;">
+            <p class="mb-0 fw-bold" style="font-size:20pt;">
+              {{ card.name }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
 
-  <el-card
+  <!-- <el-card
     shadow="hover"
     style="background:linear-gradient( to top,#f0ebe0, #f6ede9  );"
     class="mbtiCard scale-up-2"
-  >
-    <el-row style="margin-right:15px; margin-left:15px;">
-      <!-- <el-tag
-        class="mb-2"
-        effect="dark"
-        size="small"
-        style="height:20px; background:linear-gradient( to right, #D7AFA4, #E9CDA4, #B4D9A7, #87CEDC ); border:none; font-weight:600;  color:#FFFFFF; float:left;"
-      >
-        {{ card.name }}
-      </el-tag> -->
 
-      <!-- <el-tag
-        class="mb-2 scale-up-2"
-        color="#D7AFA4"
-        effect="dark"
-        size="small"
-        style="border:none; font-size:14px; height:20px; font-weight:700; border-radius: 30px; color:#FFFFFF; float:right; "
-      >
-        {{ card.title }}
-      </el-tag> -->
-    </el-row>
     <el-row
       style="margin-top:5px; display:flex; display: flex;
       justify-content: center;">
@@ -90,7 +43,7 @@
       </h3>
     </el-row>
 
-  </el-card>
+  </el-card> -->
 
 </template>
 
