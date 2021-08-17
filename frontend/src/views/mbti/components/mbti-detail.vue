4c5112435b95009055ffc6ba040772e4bfc30a58
DESKTOP-KVCQHCD\multicampus <heeya121hs@gmail.com>
2021-08-17 10:45:34 +0900

    [#25] wip: :gear: MBTI detail 수정 중

--- frontend/src/views/mbti/components/mbti-detail.vue
+++ frontend/src/views/mbti/components/mbti-detail.vue
@@ -1,83 +1,83 @@
 <template>
-  <div class="main-body main-padding">
-    <el-card class="box-card" shadow="none" style="border:none;">
+  <div class='main-body main-padding'>
+    <el-card class='box-card' shadow='none' style='border:none;'>
 
-      <div style="margin-top:40px; margin-left:60px;"></div>
+      <div style='margin-top:40px; margin-left:60px;'></div>
 
-      <el-row class="vertical-center" :gutter="20">
-        <el-col :span="12" style="margin-left:50px;">
+      <el-row class='vertical-center' :gutter='20'>
+        <el-col :span='12' style='margin-left:50px;'>
           <img
-            class="dog-thumbnail"
-            :src="state.MbtiDetail.image_url"
+            class='dog-thumbnail'
+            :src='state.MbtiDetail.image_url'
           />
         </el-col>
-        <el-col :span="12">
-          <div class="dog-info-box" style="margin-right:50px;">
+        <el-col :span='12'>
+          <div class='dog-info-box' style='margin-right:50px;'>
             <el-row>
               <el-tag
-                class="mb-2"
-                effect="dark"
-                size="small"
-                style="height:20px; background:linear-gradient( to right, #D7AFA4, #E9CDA4, #B4D9A7, #87CEDC ); border:none; font-weight:600;  color:#FFFFFF; float:left;"
+                class='mb-2'
+                effect='dark'
+                size='small'
+                style='height:20px; background:linear-gradient( to right, #D7AFA4, #E9CDA4, #B4D9A7, #87CEDC ); border:none; font-weight:600;  color:#FFFFFF; float:left;'
               >
                 {{ state.MbtiDetail.name }}
               </el-tag>
               <!-- <el-tag
-                class="mb-3  scale-up-2"
-                color="#D7AFA4"
-                effect="dark"
-                size="large"
-                style="border:none; border-radius: 30px; font-size:14pt; float:left;"
+                class='mb-3  scale-up-2'
+                color='#D7AFA4'
+                effect='dark'
+                size='large'
+                style='border:none; border-radius: 30px; font-size:14pt; float:left;'
                 >{{ state.MbtiDetail.name }}</el-tag
               > -->
             </el-row>
 
-            <div class="vertical-center row">
-              <div class="col-md-9">
-                <span :style="{ 'font-size': '30pt', 'font-weight': '700' }">{{
+            <div class='vertical-center row'>
+              <div class='col-md-9'>
+                <span :style='{ 'font-size': '30pt', 'font-weight': '700' }'>{{
                   state.MbtiDetail.title
                 }}</span>
               </div>
-              <div class="col-md-3 ms-auto">
+              <div class='col-md-3 ms-auto'>
                 <div
-                  class="align-self-center vertical-center"
-                  style="text-align: center;"
+                  class='align-self-center vertical-center'
+                  style='text-align: center;'
                 >
 
                 </div>
               </div>
             </div>
 
-            <el-divider class="mt-4 mb-4" />
+            <el-divider class='mt-4 mb-4' />
 
-            <el-descriptions class="margin-top mt-3" :column="1" :size="size">
-              <el-descriptions-item label="MBTI">
-                <el-popover placement="bottom" width="200" trigger="hover">
-                  <div class="content">
-                    <h3 style="font-weight:700;">강아지 MBTI 해석하기</h3>
+            <el-descriptions class='margin-top mt-3' :column='1' :size='size'>
+              <el-descriptions-item label='MBTI'>
+                <el-popover placement='bottom' width='200' trigger='hover'>
+                  <div class='content'>
+                    <h3 style='font-weight:700;'>강아지 MBTI 해석하기</h3>
                     <hr />
 
-                    <table class="table table-borderless">
+                    <table class='table table-borderless'>
                       <thead>
-                        <tr style="text-align:center;">
-                          <th scope="col">
-                            <h5 style="font-weight:700; color:#606266;">
+                        <tr style='text-align:center;'>
+                          <th scope='col'>
+                            <h5 style='font-weight:700; color:#606266;'>
                               활동성향
                             </h5>
                           </th>
-                          <th scope="col">
-                            <h5 style="font-weight:700; color:#606266;">
+                          <th scope='col'>
+                            <h5 style='font-weight:700; color:#606266;'>
                               순종성향
                             </h5>
                           </th>
 
-                          <th scope="col">
-                            <h5 style="font-weight:700; color:#606266;">
+                          <th scope='col'>
+                            <h5 style='font-weight:700; color:#606266;'>
                               관계성향
                             </h5>
                           </th>
-                          <th scope="col">
-                            <h5 style="font-weight:700; color:#606266;">
+                          <th scope='col'>
+                            <h5 style='font-weight:700; color:#606266;'>
                               적응성향
                             </h5>
                           </th>
@@ -87,42 +87,42 @@
                         <tr>
                           <td>
                             <el-tag
-                              class="m-3"
-                              color="#D7AFA4"
-                              effect="dark"
-                              size="large"
-                              :style="{ border: '3px solid #D7AFA4' }"
+                              class='m-3'
+                              color='#D7AFA4'
+                              effect='dark'
+                              size='large'
+                              :style='{ border: '3px solid #D7AFA4' }'
                               >E</el-tag
                             >에너지 (E) Energetic
                           </td>
                           <td>
                             <el-tag
-                              class="m-3"
-                              color="#E9CDA4"
-                              effect="dark"
-                              size="large"
-                              :style="{ border: '3px solid #E9CDA4' }"
+                              class='m-3'
+                              color='#E9CDA4'
+                              effect='dark'
+                              size='large'
+                              :style='{ border: '3px solid #E9CDA4' }'
                               >S</el-tag
                             >충성심 강한 (S) Supportive
                           </td>
 
                           <td>
                             <el-tag
-                              class="m-3"
-                              color="#B4D9A7"
-                              effect="dark"
-                              size="large"
-                              :style="{ border: '3px solid #B4D9A7' }"
+                              class='m-3'
+                              color='#B4D9A7'
+                              effect='dark'
+                              size='large'
+                              :style='{ border: '3px solid #B4D9A7' }'
                               >F</el-tag
                             >관계지향 (F) Friendly
                           </td>
                           <td>
                             <el-tag
-                              class="m-3"
-                              color="#87CEDC"
-                              effect="dark"
-                              size="large"
-                              :style="{ border: '3px solid #87CEDC' }"
+                              class='m-3'
+                              color='#87CEDC'
+                              effect='dark'
+                              size='large'
+                              :style='{ border: '3px solid #87CEDC' }'
                               >P</el-tag
                             >신중한 (P) Prudent
                           </td>
@@ -130,42 +130,42 @@
                         <tr>
                           <td>
                             <el-tag
-                              class="m-3"
-                              color="#FFFFFF"
-                              style="border: 3px solid #D7AFA4; color: #606266;"
-                              effect="dark"
-                              size="large"
+                              class='m-3'
+                              color='#FFFFFF'
+                              style='border: 3px solid #D7AFA4; color: #606266;'
+                              effect='dark'
+                              size='large'
                               >I</el-tag
                             >침착한 (I) Introversion
                           </td>
                           <td>
                             <el-tag
-                              class="m-3"
-                              color="#FFFFFF"
-                              style="border: 3px solid #E9CDA4; color: #606266;"
-                              effect="dark"
-                              size="large"
+                              class='m-3'
+                              color='#FFFFFF'
+                              style='border: 3px solid #E9CDA4; color: #606266;'
+                              effect='dark'
+                              size='large'
                               >N</el-tag
                             >영리한 (N) Naughty
                           </td>
 
                           <td>
                             <el-tag
-                              class="m-3"
-                              color="#FFFFFF"
-                              style="border: 3px solid #B4D9A7; color: #606266;"
-                              effect="dark"
-                              size="large"
+                              class='m-3'
+                              color='#FFFFFF'
+                              style='border: 3px solid #B4D9A7; color: #606266;'
+                              effect='dark'
+                              size='large'
                               >T</el-tag
                             >독립지향 (T) independenT
                           </td>
                           <td>
                             <el-tag
-                              class="m-3"
-                              color="#FFFFFF"
-                              style="border: 3px solid #87CEDC; color: #606266;"
-                              effect="dark"
-                              size="large"
+                              class='m-3'
+                              color='#FFFFFF'
+                              style='border: 3px solid #87CEDC; color: #606266;'
+                              effect='dark'
+                              size='large'
                               >J</el-tag
                             >친화적인 (J) Jolly
                           </td>
@@ -176,22 +176,22 @@
 
                   <template #reference>
                     <i
-                      class="el-icon-question scale-up-2"
-                      style="margin-left : 10px; cursor: pointer;"
+                      class='el-icon-question scale-up-2'
+                      style='margin-left : 10px; cursor: pointer;'
                     />
                   </template>
                 </el-popover>
               </el-descriptions-item>
             </el-descriptions>
 
-            <el-divider class="mb-3" />
+            <el-divider class='mb-3' />
 
 
           </div>
         </el-col>
       </el-row>
-      <div class="box">
-        <h6 class="mb-2" style="line-height: 3rem;">
+      <div class='box'>
+        <h6 class='mb-2' style='line-height: 3rem;'>
           {{ state.MbtiDetail.desc }}
         </h6>
       </div>
@@ -203,6 +203,48 @@
 
 
     <!-- 유사 공고 -->
+
+
+
+        <div class='dog-image-box'>
+          <h5
+            class='pt-3 pb-3'
+            style='font-weight:600; padding-left:20px; background:linear-gradient( to bottom,#f3e8dc, #f5edea );'
+          >
+            🐶🔎 저와 같은 MPTI를 가진 친구는 누가 있을까요❔
+          </h5>
+          <!-- 유사공고 -->
+          <div v-if='state.listSimilarDog != null'>
+            <el-scrollbar>
+              <div class='flex-content'>
+                <p
+                  class='item'
+                  v-for='(card, idx) in state.listSimilarDog'
+                  :key='idx'
+                  style='width:360px; margin: 5px; display:inline-block'
+                >
+                  <FindCard
+                    :card='card'
+                    @click='readDetail(card.boardId.id)'
+                    style='margin:10px; width:360px;'
+                  />
+                </p>
+              </div>
+            </el-scrollbar>
+          </div>
+
+          <div v-else>
+            <el-empty
+              description='유사한 강아지가 없네요😢'
+              image='https://d2ud6j7vlf3xy9.cloudfront.net/img/KakaoTalk_20210816_223416590.png'
+              image-size='300'
+            >
+            </el-empty>
+          </div>
+        </div>
+
+
+
   </div>
 </template>
 
@@ -229,7 +271,33 @@ export default {
       })
     });
 
-    return { state, };
+    const kakaoShare = function() {
+      Kakao.Link.sendDefault({
+        objectType: 'mpti',
+        content: {
+          title: state.MbtiDetail.title,
+          desc: state.MbtiDetail.desc,
+          imageUrl: state.MbtiDetail.image_url,
+          link: {
+            mobileWebUrl: 'https://i5a501.p.ssafy.io/',
+            webUrl: 'https://i5a501.p.ssafy.io/'
+          }
+        },
+        buttons: [
+          {
+            title: '독립으로 이동',
+            link: {
+              mobileWebUrl: 'https://i5a501.p.ssafy.io/',
+              webUrl: 'https://i5a501.p.ssafy.io/'
+            }
+          }
+        ]
+      });
+    };
+
+
+
+    return { state, kakaoShare, };
   }
 };
 </script>
