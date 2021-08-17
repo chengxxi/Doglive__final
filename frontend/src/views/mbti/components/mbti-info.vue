ed9833237b2d7c25fafb26a65f5dda3bdfdbf26f
Daun jo <jodawooooon@naver.com>
2021-08-17 14:26:26 +0900

    [#25] style: :art: mati card 디테일 수정

--- frontend/src/views/mbti/components/mbti-info.vue
+++ frontend/src/views/mbti/components/mbti-info.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="main-body main-padding">
     <div class="box-card " style="width:100%; border:none;">
-      <div style="margin-top:50px">
-        <h1 class="title">나와 맞는 강아지 찾기: information</h1>
+      <div style="margin-top:50px ">
+        <h5 class="fw-bold mb-0">16 Pupsonality</h5>
+        <h1 class="fw-bold mt-0" style="font-size:60pt; ">
+          MPTI
+        </h1>
+        <h4 class="fw-bold mt-0">My Puppy Type Indicator</h4>
         <p>강아지 성향 기반 MBTI를 통한 나와 맞는 성향의 강아지 찾기</p>
         <!-- 상세 문구 수정 필요 -->
       </div>
 
       <MbtiResult />
-
     </div>
   </div>
 </template>
-
 
 <style>
 /* 페이지 만들 때, 이 구조가 기준이 됩니다! (양옆 여백 10%, 위 여백 50px) */
@@ -29,20 +31,15 @@
   font-size: 2.5rem;
   font-weight: normal;
 }
-
-
 </style>
 
-
-
 <script>
-import MbtiResult from './mbti-result.vue'
-
+import MbtiResult from "./mbti-result.vue";
 
 export default {
-  name: 'mbti-info',
+  name: "mbti-info",
   components: {
-    MbtiResult,
-  },
-}
+    MbtiResult
+  }
+};
 </script>
