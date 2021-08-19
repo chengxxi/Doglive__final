d331708bbb2d9a072c4b1a8236fb4639c1372d9c
Daun jo <jodawooooon@naver.com>
2021-08-19 23:37:16 +0900

    [#47] bug: :bug: 버튼 오타 수정

--- frontend/src/views/mbti/components/mbti-intro.vue
+++ frontend/src/views/mbti/components/mbti-intro.vue
@@ -1,36 +1,41 @@
 <template>
-<div>
-  <div class="box">
-    <h1 class="description">
-      🐕 My Puppy Type Indicator 🐾
-    </h1>
+  <div>
+    <div class="box">
+      <h1 class="description">
+        🐕 My Puppy Type Indicator 🐾
+      </h1>
 
-    <img src="https://storage.googleapis.com/neris/public/images/articles/pets.svg" class="img"/>
-    <p>
-      "독립"의 <b>'MPTI(My Puppy Type Indicator)'</b>는 강아지들의 성격을 총 16가지로 분류해 정리했어요. <br>
+      <img
+        src="https://storage.googleapis.com/neris/public/images/articles/pets.svg"
+        class="img"
+      />
+      <p>
+        "독립"의 <b>'MPTI(My Puppy Type Indicator)'</b>는 강아지들의 성격을 총
+        16가지로 분류해 정리했어요. <br />
 
-      원하는 강아지의 모습을 머릿속에 그리며, 하나씩 질문에 응답하다 보면 나와 꼭 맞는 강아지를 추천받을 수 있답니다! <br>
+        원하는 강아지의 모습을 머릿속에 그리며, 하나씩 질문에 응답하다 보면 나와
+        꼭 맞는 강아지를 추천받을 수 있답니다! <br />
 
-      "독립"의 'MPTI'를 통해 <b style="color: #D7AFA4;">새롭게 우리 가족이 될 강아지</b>를 발견해 보세요. <br>
-    </p>
+        "독립"의 'MPTI'를 통해
+        <b style="color: #D7AFA4;">새롭게 우리 가족이 될 강아지</b>를 발견해
+        보세요. <br />
+      </p>
 
-
-    <!-- <a href="https://www.16personalities.com/articles/introducing-16pet-sonalities"><span style="float: right; font-weight: 300;">16 pet-sonalities</span></a> -->
-  </div>
-
-  <div class="mbtiBtn">
-      <!-- round option -->
-      <a href="/mbti/test">
-        <el-button class="goMbtiBtn" round>MBTI 테스트 하러 가기</el-button>
-      </a>
-      <a href="/mbti/info">
-        <el-button class="showMbtiBtn" round>독립 MBTI 테스트 유형 보기</el-button>
-      </a>
-
+      <!-- <a href="https://www.16personalities.com/articles/introducing-16pet-sonalities"><span style="float: right; font-weight: 300;">16 pet-sonalities</span></a> -->
     </div>
 
-</div>
-
+    <div class="mbtiBtn">
+      <!-- round option -->
+      <a href="/mbti/test">
+        <el-button class="goMbtiBtn" round>MPTI 테스트 하러 가기</el-button>
+      </a>
+      <a href="/mbti/info">
+        <el-button class="showMbtiBtn" round
+          >독립 MPTI 테스트 유형 보기</el-button
+        >
+      </a>
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -77,28 +82,24 @@
 }
 
 .goMbtiBtn {
-  border: 2px solid #E9CDA4;
-  background-color: #E9CDA4;
+  border: 2px solid #e9cda4;
+  background-color: #e9cda4;
   font-weight: 600;
   color: white;
   margin: 0 15px;
 }
 
 .showMbtiBtn {
-  border: 2px solid #D7AEA4;
-  background-color: #D7AEA4;
+  border: 2px solid #d7aea4;
+  background-color: #d7aea4;
   font-weight: 600;
   color: white;
   margin: 0 15px;
 }
-
-
 </style>
 
 <script>
 export default {
-  name: 'MbtiIntro',
-}
+  name: "MbtiIntro"
+};
 </script>
-
-
